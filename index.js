1cdfbb6bfdad89e65679a54f17a5137298c0b458
josearlindodasilva07-svg <josearlindodasilva07@gmail.com>
2026-08-07 13:56:09 -0300

    Add 'kkkk' to the default blacklist

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ let serverLogs = [];
 
 // Blacklist padrão (já inclui alguns jogadores conhecidos)
 const DEFAULT_BLACKLIST = [
-    "brainrot_21599",
+    "kkkk",
     "jogador_traidor",
     // Adicione aqui os jogadores que você quer banir
 ];
